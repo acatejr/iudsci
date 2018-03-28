58eab5db6ec44cf0a85d7a2e03b7585e83353952
Averill Cate, Jr <acatejr@gmail.com>
2018-03-27 18:52:44 -0700

    Assignment 2 problem 4.

--- spring2018/python_machine_learning/module5/assignment2.py
+++ spring2018/python_machine_learning/module5/assignment2.py
@@ -48,13 +48,34 @@ def expired_meds(meds):
         if med_exp_date < datetime.datetime.now():
             print("{} expired on {}".format(med_name, meds[m]))
 
+def dec_to_bin_rec(n):
+    """ Recursive function for converting decimal to binary.
+    """    
+    # Base case
+    if n < 1:
+        return n % 2
+    else:
+        q = n // 2        
+        r = n % 2
+        return str(dec_to_bin_rec(q)) + str(r)
+
 if __name__ == '__main__':
+    # Dec to bin iterative
     print("The integer 12 in binary is {}.".format(dec_to_bin_iter(12)))
     print("The integer 5 in binary is {}.".format(dec_to_bin_iter(5)))
     print("The integer 293 in binary is {}.".format(dec_to_bin_iter(293)))
 
+    # Bin to dec iterative
     print("Binary 1100 in binary is {}.".format(arr_to_dec([1,1,0,0])))
     print("Binary 101 in binary is {}.".format(arr_to_dec([1,0,1])))
     print("Binary 100100101 in binary is {}.".format(arr_to_dec([1,0,0,1,0,0,1,0,1])))
 
+    # Expired meds
     expired_meds(my_med_dict)
+
+    print("\n")
+    # Dec to bin recursive
+    print("The integer 12 in binary is {}.".format(dec_to_bin_rec(12)[1:]))
+    print("The integer 5 in binary is {}.".format(dec_to_bin_rec(5))[1:])
+    print("The integer 293 in binary is {}.".format(dec_to_bin_rec(293)[1:]))
+    print("The integer 9 in binary is {}.".format(dec_to_bin_rec(9)[1:]))
