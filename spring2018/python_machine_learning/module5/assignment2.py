b37a35ffe23fb9ba2cf8a2dd51cb26d448d48268
Averill Cate, Jr <acatejr@gmail.com>
2018-03-27 20:09:41 -0700

    Assignment 2 problem 5.

--- spring2018/python_machine_learning/module5/assignment2.py
+++ spring2018/python_machine_learning/module5/assignment2.py
@@ -1,6 +1,26 @@
 # assignment2.py
 # author: Averill Cate, Jr
-import datetime
+import datetime, os
+import numpy as np
+import pandas as pd
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+CRIME_DATA_COLUMNS = [
+    'ADDRESS', 'ARSON', 'ASSAULT', 'BAD_CHECKS', 
+    'BRIBERY', 'BURGLARY', 'DISORDERLY_CONDUCT', 
+    'DRIVING_UNDER_THE_INFLUENCE', 'DRUG/NARCOTIC', 
+    'DRUNKENNESS', 'EMBEZZLEMENT', 'EXTORTION', 
+    'FAMILY_OFFENSES', 'FORGERY/COUNTERFEITING', 
+    'FRAUD', 'GAMBLING', 'KIDNAPPING', 
+    'LARCENY/THEFT', 'LIQUOR_LAWS', 'LOITERING', 
+    'MISSING_PERSON', 'NON_CRIMINAL', 'OTHER_OFFENSES', 
+    'PORNOGRAPHY/OBSCENE_MAT', 'PROSTITUTION', 'RECOVERED_VEHICLE', 
+    'ROBBERY', 'RUNAWAY', 'SECONDARY_CODES', 'SEX_OFFENSES_FORCIBLE', 
+    'SEX_OFFENSES_NON_FORCIBLE', 'STOLEN_PROPERTY', 'SUICIDE', 
+    'SUSPICIOUS_OCC', 'TREA', 'TRESPASS', 'VANDALISM', 'VEHICLE_THEFT', 'WARRANTS', 
+    'WEAPON LAWS'
+]
 
 # Data for problem 3
 my_med_dict = {
@@ -59,6 +79,19 @@ def dec_to_bin_rec(n):
         r = n % 2
         return str(dec_to_bin_rec(q)) + str(r)
 
+def crimes(crime):
+    """ Problem 5.  Calculates the mean of that crime, i.e., average number 
+    of times that crime has happened at addresses.  The function should return 
+    all the address (may be in a list) where this crime has occurred more than 
+    the mean number of times.
+    """
+    file_name = BASE_DIR + '/crime.csv'
+    data = pd.read_csv(file_name, sep=',')
+    data.columns = CRIME_DATA_COLUMNS
+    avg = data[crime].mean()
+    results = data[data[crime] > avg]
+    return results.ADDRESS.values
+
 if __name__ == '__main__':
     # Dec to bin iterative
     print("The integer 12 in binary is {}.".format(dec_to_bin_iter(12)))
@@ -79,3 +112,18 @@ if __name__ == '__main__':
     print("The integer 5 in binary is {}.".format(dec_to_bin_rec(5))[1:])
     print("The integer 293 in binary is {}.".format(dec_to_bin_rec(293)[1:]))
     print("The integer 9 in binary is {}.".format(dec_to_bin_rec(9)[1:]))
+
+    # Crimes
+    crime_name = 'EXTORTION'
+    print("\nThe following addresses exceed the average rate for {}".format(crime_name))
+    print("="*70)
+    addresses = crimes(crime_name)
+    for a in addresses:
+        print(a)
+
+    crime_name = 'DRUNKENNESS'
+    print("\nThe following addresses exceed the average rate for {}".format(crime_name))
+    print("="*70)
+    addresses = crimes(crime_name)
+    for a in addresses:
+        print(a)
