ef9895dcf06ba2994255f9d5ff4cdd9e412b64d3
Averill Cate, Jr <acatejr@gmail.com>
2018-04-06 12:34:11 -0700

    Intro to sql assignment 4 work.

---
+++ spring2018/intro_to_sql/assignment4_data.sql
@@ -0,0 +1,11 @@
+insert into customers values 
+(41, 'Suhas Jagadish', '720 S College Mall Rd', 'Bloomington', 47401, 'USA'),
+(42, 'Walter White', '2411 E 2nd St.', 'Bloomington', 47401, 'USA'),
+(43, 'Jessy Pinkman', '1320 S Walnut Road', 'Chicago', 60290, 'USA'),
+(44, 'Arjun Rao', '240 N Harrison St', 'Bloomington', 47401, 'USA'),
+(45, 'Saul Goodman', '233 S Wacker Dr', 'Chicago', 60290, 'USA');
+
+insert into orders values 
+(101, 'Earphones', 41, '6/25/2016', 25),
+(102, 'Sony Playstation', 43, '5/20/2016', 300),
+(103, 'Nexus 5x', 44, '12/30/2015', 429);
